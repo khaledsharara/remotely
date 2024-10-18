331922fd40f95605f08225000387fc1872c46c91
Khaled <166764911+khaledsharara@users.noreply.github.com>
2024-10-18 05:43:51 +0300

    util: add user slice

--- remotely/modules/App.tsx
+++ remotely/modules/App.tsx
@@ -1,8 +1,14 @@
+import { Provider } from "react-redux";
 import "./App.css";
 import AppRouter from "./AppRouter";
+import store from "./shared/utils/store";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
 }
 
 export default App;
