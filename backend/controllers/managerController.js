09f6980a6a9f3ea00a2d379543fd7c166e620469
menna <mennasherif0330@outlook.com>
2024-12-05 23:26:32 +0200

    feat: added create manager

--- backend/controllers/managerController.js
+++ backend/controllers/managerController.js
@@ -27,3 +27,43 @@ exports.addTask = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 };
+
+exports.createManager = async (req, res) => {
+  try {
+    const { email, password, name, role } = req.body;
+
+    if (!email || !password || !name || !role) {
+      return res.status(400).json({ error: "All fields are required." });
+    }
+
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const { uid } = userCredential.user;
+
+    const newEmployee = {
+      uid,
+      name,
+      email,
+      role,
+    };
+
+    const employeeRef = db.ref("managers").child(uid);
+    await employeeRef.set(newEmployee);
+
+    return res.status(200).json({
+      message: "Employee created successfully",
+      data: {
+        uid,
+        name,
+        email,
+        role,
+      },
+    });
+  } catch (error) {
+    console.error("Error creating employee:", error);
+    return res.status(500).json({ error: error.message });
+  }
+};
--- backend/routes/managerRoutes.js
+++ backend/routes/managerRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const { addTask } = require("../controllers/managerController");
+const { addTask, createManager } = require("../controllers/managerController");
 
 router.post("/:id/add-task", addTask);
+router.post("/create", createManager);
 
 module.exports = router;
