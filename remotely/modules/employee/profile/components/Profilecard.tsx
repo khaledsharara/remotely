c7afa965a4059094b24d29f36931f25449280049
menna <mennasherif0330@outlook.com>
2024-12-06 12:56:29 +0200

    util: added info in profile card

--- remotely/modules/employee/profile/components/Profilecard.tsx
+++ remotely/modules/employee/profile/components/Profilecard.tsx
@@ -1,23 +1,23 @@
 import { FaArrowRightLong } from "react-icons/fa6";
 import profile from "../../../../public/profile.svg";
 import "../styles/ProfileCard.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../../shared/utils/userSlice";
 
 function ProfileCard() {
-  // Static filler data for profile
-  const name = "John Doe"; // Replace with a static name
-  const email = "johndoe@example.com"; // Replace with a static email
+  const user = useSelector(selectUser);
 
   return (
     <div className="profile-card-root">
       <div className="profile-card-body">
         {/* name */}
-        <div className="profile-card-body-header">{name}</div>
+        <div className="profile-card-body-header">{user.name}</div>
         {/* content */}
         <div className="profile-card-body-content">
-          <div className="profile-card-body-type">Student</div>
-          {email ? (
+          <div className="profile-card-body-type">{user.employeeRole}</div>
+          {user.email ? (
             <div className="profile-card-body-email">
-              <div className="profile-card-body-email-text">{email}</div>
+              <div className="profile-card-body-email-text">{user.email}</div>
             </div>
           ) : (
             <div className="profile-card-body-button">
